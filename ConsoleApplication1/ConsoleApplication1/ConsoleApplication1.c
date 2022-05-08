6d9c45f6182ea763353ba4def3b1e13ec2260566
M0000000N <103083005+M0000000N@users.noreply.github.com>
2022-05-09 03:02:20 +0900

    docs
    220508

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.c
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.c
@@ -1,4 +1,7 @@
-﻿#include <stdio.h>
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h> // malloc, free
+#include <string.h> // memset
 #pragma region 
 //int main()
 //{
@@ -354,9 +357,9 @@
 //        i++;
 //    }
 //
-//    return 0;
+//    return 0; 30.6 연습문제 : 3으로 끝나는 숫자만 출력
 //}
-#pragma endregion 30.6 연습문제 3으로 끝나는 숫자만 출력하기
+#pragma endregion 
 
 #pragma region 
 //int main()
@@ -406,7 +409,7 @@
 //}
 #pragma endregion 33.5 코드 단축하기
 
-#pragma region 
+#pragma region  
 //int main()
 //{
 //    for (int i = 1; i <= 100; i++)
@@ -420,9 +423,9 @@
 //        else
 //            printf("%d\n", i);
 //    }
-//    return 0;
+//    return 0; 33.7 연습문제 : 2와 7의 배수, 공배수 처리하기
 //}
-#pragma endregion 33.7 연습문제 : 2와 7의 배수, 공배수 처리하기
+#pragma endregion
 
 #pragma region 
 //int main()
@@ -441,29 +444,246 @@
 //}
 #pragma endregion 34.3 디버거에서 포인터 확인하기
 
-#pragma region 
+#pragma region
+//int main()
+//{
+//    int num1 = 20;
+//    int* numPtr1;
+//
+//    numPtr1 = &num1;
+//
+//    int* numPtr2;
+//
+//    numPtr2 = malloc(sizeof(int));  // int의 크기 4바이트만큼 동적 메모리 할당
+//
+//    printf("%p\n", numPtr1);
+//
+//    printf("%p\n", numPtr2);
+//
+//    free(numPtr2);              // malloc 사용해서 힙에서 할당한 메모리는 free함수로 메모리를 해제해야함.
+//
+//    return 0;
+//}
+#pragma endregion 35.1 메모리 할당하기
+
+#pragma region
+//int main()
+//{
+//    long long* numPtr = malloc(sizeof(long long));  // long long의 크기 8바이트만큼 동적 메모리 할당
+//
+//    memset(numPtr, 0x27, 8);    // (포인터, 설정할 값, 크기)
+//
+//    printf("0x%llx\n", *numPtr);    // 0x2727272727272727: 27이 8개 들어가 있음
+//
+//    free(numPtr);    // 동적으로 할당한 메모리 해제
+//
+//    return 0;
+//}
+#pragma endregion 35.3 메모리 내용을 한꺼번에 설정하기
+
+#pragma region
+//int main()
+//{
+//    float scores[10] = { 67.2f, 84.3f, 97.0f, 87.1f, 71.9f, 63.0f, 90.1f, 88.0f, 79.7f, 95.3f };
+//    float sum = 0.0f;
+//    float average;
+//
+//    for (int i = 0; i < sizeof(scores) / sizeof(float); i++)
+//    {
+//        sum += scores[i];
+//    }
+//
+//    average = sum / 10;
+//
+//        printf("%f\n", average);
+//
+//    return 0;
+//}
+#pragma endregion 36.11 연습문제 : 점수평균 구하기
+
+#pragma region
+//int main()
+//{
+//    int decimal = 0;
+//    int binary[4] = { 1, 1, 0, 1 };    // 1101 순서대로 저장됨
+//
+//    int num = 0;
+//    for (int i = sizeof(binary) / sizeof(int) - 1; i >= 0; --i)
+//    {
+//        if (binary[i] == 1)
+//        {
+//            decimal += 1 << num;
+//        }
+//        num++;
+//    }
+//
+//        printf("%d\n", decimal);
+//
+//    return 0;
+//}
+#pragma endregion 36.12 연습문제 : 이진수-> 십진수
+
+#pragma region
+//int main()
+//{
+//    int numArr[3][4] = {    // 세로 3, 가로 4 크기의 int형 2차원 배열 선언
+//        { 11, 22, 33, 44 },
+//        { 55, 66, 77, 88 },
+//        { 99, 110, 121, 132 }
+//    };
+//
+//    int(*numPtr)[4] = numArr;
+//
+//    printf("%p\n", *numPtr); // 002BFE5C: 2차원 배열 포인터를 역참조하면 세로 첫 번째의 주소가 나옴
+//                             // 컴퓨터마다, 실행할 때마다 달라짐
+//
+//    printf("%p\n", *numArr); // 002BFE5C: 2차원 배열을 역참조하면 세로 첫 번째의 주소가 나옴
+//                             // 컴퓨터마다, 실행할 때마다 달라짐
+//
+//    printf("%d\n", numPtr[2][1]);    // 110: 2차원 배열 포인터는 인덱스로 접근할 수 있음
+//
+//    printf("%d\n", sizeof(numArr));  // 48: sizeof로 2차원 배열의 크기를 구하면 배열이 메모리에 
+//                                     // 차지하는 공간이 출력됨
+//
+//    printf("%d\n", sizeof(numPtr));  // 4 : sizeof로 2차원 배열 포인터의 크기를 
+//                                     // 구하면 포인터의 크기가 출력됨(64비트라면 8)
+//
+//    return 0;
+//}
+#pragma endregion 37.6 이차워 배열을 포인터에 넣기
+
+#pragma region
+//int main()
+//{
+//    int matrix[8][8] = {
+//        {  1,  2,  3,  4,  5,  6,  7,  8 },
+//        {  9, 10, 11, 12, 13, 14, 15, 16 },
+//        { 17, 18, 19, 20, 21, 22, 23, 24 },
+//        { 25, 26, 27, 28, 29, 30, 31, 32 },
+//        { 33, 34, 35, 36, 37, 38, 39, 40 },
+//        { 41, 42, 43, 44, 45, 46, 47, 48 },
+//        { 49, 50, 51, 52, 53, 54, 55, 56 },
+//        { 57, 58, 59, 60, 61, 62, 63, 64 }
+//    };
+//
+//    for (int i = 0; i < sizeof(matrix) / sizeof(matrix[0]); ++i)
+//    {
+//        printf("%d ", matrix[i][i]);
+//    }
+//        return 0;
+//}
+#pragma endregion 37.8 연습문제 : 행렬의 주대각선 성분 구하기
+
+#pragma region
+//int main()
+//{
+//    int* numPtr = malloc(sizeof(int) * 10);    // int 10개 크기만큼 동적 메모리 할당
+//
+//    numPtr[0] = 10;    // 배열처럼 인덱스로 접근하여 값 할당
+//    numPtr[9] = 20;    // 배열처럼 인덱스로 접근하여 값 할당
+//
+//    printf("%d\n", numPtr[0]);    // 배열처럼 인덱스로 접근하여 값 출력
+//    printf("%d\n", numPtr[9]);    // 배열처럼 인덱스로 접근하여 값 출력
+//
+//    free(numPtr);    // 동적으로 할당한 메모리 해제
+//
+//    return 0;
+//}
+#pragma endregion 38.1 포인터에 할당된 메모리를 배열처럼 사용하기
+
+#pragma region
+//int main()
+//{
+//	int** m = malloc(sizeof(int*) * 3); //세로공간 메모리 할당
+//
+//	for (int i = 0; i < 3; ++i) //가로공간 메모리 할당
+//	{
+//		m[i] = malloc(sizeof(int) * 4);
+//	}
+//
+//	m[0][0] = 1;
+//	m[2][0] = 5;
+//	m[2][3] = 2;
+//
+//	printf("%d\n", m[0][0]);
+//	printf("%d\n", m[2][0]);
+//	printf("%d\n", m[2][3]);
+//
+//	for (int i = 0; i < 3; ++i) // 세로공간 먼저 해제하면 가로공간 메모리 해제 못함
+//	{
+//		free(m[i]);
+//	}
+//
+//	free(m); // 세로공간 메모리 해제
+//
+//}
+#pragma endregion 38.2 포인터에 할당된 메모리를 이차원 배열처럼 사용하기
+
+#pragma region
+//int main()
+//{
+//	int row, col;
+//
+//	scanf("%d %d", &row, &col);
+//
+//	int** m = malloc(sizeof(int*) * row);
+//
+//	for (int i = 0; i < row; ++i)
+//	{
+//		m[i] = malloc(sizeof(int) * col);
+//	}
+//
+//	for (int i = 0; i < row; i++)
+//	{
+//		for (int j = 0; j < col; j++)
+//		{
+//			m[i][j] = i + j;
+//		}
+//	}
+//	
+//	for (int i = 0; i < row; i++)
+//	{
+//		for (int j = 0; j < col; ++j)
+//		{
+//			printf("%d", m[i][j]);
+//		}
+//		printf("\n");
+//	}
+//
+//	for (int i = 0; i < row; ++i)
+//	{
+//		free(m[i]);
+//	}
+//
+//	free(m);
+//
+//	return 0;
+//}
+#pragma endregion 38.2 포인터에 할당된 메모리를 이차원 배열처럼 사용하기
+
+#pragma region
 int main()
 {
-    long long* numPtr1;    // long long형 포인터 선언
-    float* numPtr2;        // float형 포인터 선언
-    char* cPtr1;           // char형 포인터 선언
+    long long*** m = malloc(sizeof(long long**) * 2); // 높이 메모리공간 할당
+    
+    for (int row = 0; row < 2; row++) // 세로 메모리 공간 할당
+    {
+        m[row] = malloc(sizeof(long long*) * 3);
 
-    long long num1 = 10;
-    float num2 = 3.5f;
-    char c1 = 'a';
+        for (int col = 0; col < 3; col++)
+        {
+            m[row][col] = malloc(sizeof(long long) * 5);
+        }
+    }
 
-    numPtr1 = &num1;    // num1의 메모리 주소 저장
-    numPtr2 = &num2;    // num2의 메모리 주소 저장
-    cPtr1 = &c1;        // c1의 메모리 주소 저장
+        m[1][2][4] = 100;
 
-    printf("%d\n", *numPtr1);    // 10
-    printf("%f\n", *numPtr2);      // 3.500000
-    printf("%c\n", *cPtr1);        // a
+    printf("%lld\n", m[1][2][4]);
+
+    for (int col = 0; col < 3; )
+
+        free(m);
 
     return 0;
 }
-#pragma endregion 34.3 디버거에서 포인터 확인하기
-
-
-
-
+#pragma endregion 38.6 연습문제 : 포인터에 할당된 메모리를 삼차원 배열처럼 사용하기
